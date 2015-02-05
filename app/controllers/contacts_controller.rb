bc67825bd0129a5d52c636f746519d91c0006ad6
MrBrownser <castany.adria@gmail.com>
2015-02-05 01:29:49 +0100

    Added some validations to the views and controllers, handling exceptions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,12 @@ class ContactsController < ApplicationController
 		# @contacts = Contacts.order(created_at: DESC)
 		@contacts = Contact.all
 	end
+
+	def show
+		begin
+			@contact = Contact.find params[:id]
+		rescue ActiveRecord::RecordNotFound
+			render 'no_contact_found'
+		end
+	end
 end
