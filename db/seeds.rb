fedf85a71a5803d500c57feaa8ad68241738f0f1
MrBrownser <castany.adria@gmail.com>
2015-02-05 01:22:32 +0100

    Added some seeds and improved the initial contact list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Contact.create name: "Adrià", address: "C/Bla, Barcelona"
+Contact.create name: "Hector", address: "C/Bla, Canarias"
+Contact.create name: "Gorka", address: "C/Bla, Valencia"
+Contact.create name: "Helena", address: "C/Bla, Tarragona"
+Contact.create name: "Amor", address: "C/Bla, Venezuela"
+Contact.create name: "Mingos", address: "C/Bla, Galicia"
