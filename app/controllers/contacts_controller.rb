4e315e4a8125c0df9af5cf6639d2637e77a278ab
MrBrownser <castany.adria@gmail.com>
2015-02-15 15:22:31 +0100

    Restarted doing this app

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,22 +6,10 @@ class ContactsController < ApplicationController
 	def show
 		begin
 			@contact = Contact.find params[:id]
+			@phones = @contact.phones
+			@emails = @contact.emails
 		rescue ActiveRecord::RecordNotFound
 			render 'no_contact_found'
 		end
 	end
 end
-
-__END__
-
-# <address>
-#   <strong>Twitter, Inc.</strong><br>
-#   795 Folsom Ave, Suite 600<br>
-#   San Francisco, CA 94107<br>
-#   <abbr title="Phone">P:</abbr> (123) 456-7890
-# </address>
-
-# <address>
-#   <strong>Full Name</strong><br>
-#   <a href="mailto:#">first.last@example.com</a>
-# </address>
